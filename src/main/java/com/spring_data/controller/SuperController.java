f0d33717c78f0ff56599899ed3667be0b2a83a23
Igor <Sketch_90@mail.ru>
2023-01-30 10:02:18 +0300

    First Commit

---
+++ src/main/java/com/spring_data/controller/SuperController.java
@@ -0,0 +1,7 @@
+package com.spring_data.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SuperController {
+}
---
+++ src/main/java/com/spring_data/repository/UserRepo.java
@@ -0,0 +1,7 @@
+package com.spring_data.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo {
+}
